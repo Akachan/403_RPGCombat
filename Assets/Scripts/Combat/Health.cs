d9144200c2c1f75b550acb715e4d0f14df2cb234
Akachan <chaves.mayra@gmail.com>
2024-02-08 15:08:38 -0300

    Trigger Death Animations

--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -7,6 +7,8 @@ namespace RPG.Combat
     {
         [SerializeField] private float health =100f;
         private float _currentHealth;
+        private bool isDead;
+        private static readonly int Death = Animator.StringToHash("death");
 
         private void Start()
         {
@@ -17,6 +19,11 @@ namespace RPG.Combat
         {
             _currentHealth = Mathf.Max(_currentHealth - damage, 0);
             print($"Enemy Health: {_currentHealth}");
+
+            if (_currentHealth != 0 || isDead) return;
+            GetComponent<Animator>().SetTrigger(Death);
+            isDead = true;
+
         }
     }
 }
