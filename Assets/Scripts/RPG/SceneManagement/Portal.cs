dcd8d769719f556fb0bf0a0a803a67672bc8e0f9
Akachan <chaves.mayra@gmail.com>
2024-02-16 17:03:20 -0300

    Scene Loading Portals

---
+++ Assets/Scripts/RPG/SceneManagement/Portal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RPG.SceneManagement
+{
+    public class Portal : MonoBehaviour
+    {
+        [SerializeField] private int sceneToLoad;
+        private void OnTriggerEnter(Collider other)
+        {
+            if(!other.CompareTag("Player")) {return;}
+
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
