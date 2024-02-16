3f2bbaf6528fc95997db2a417be01c724a6de567
Akachan <chaves.mayra@gmail.com>
2024-02-16 19:23:24 -0300

    PlayerSpawnPoint

--- Assets/Scripts/RPG/SceneManagement/Portal.cs
+++ Assets/Scripts/RPG/SceneManagement/Portal.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections;
+using RPG.Control;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 
 namespace RPG.SceneManagement
@@ -8,7 +10,7 @@ namespace RPG.SceneManagement
     public class Portal : MonoBehaviour
     {
         [SerializeField] private int sceneToLoad;
-
+        [field: SerializeField] public Transform SpawnPoint { get; private set; }
  
         private void OnTriggerEnter(Collider other)
         {
@@ -21,8 +23,36 @@ namespace RPG.SceneManagement
         {
             DontDestroyOnLoad(gameObject);
             yield return SceneManager.LoadSceneAsync(sceneToLoad);
-            print("scene loaded");
+            
+            var otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+            
             Destroy(gameObject);
+        }
+
+        private void UpdatePlayer(Portal otherPortal)
+        {
+            if (otherPortal == null ) {return;}
+            
+            var player = GameObject.FindWithTag("Player");
+            player.GetComponent<NavMeshAgent>().Warp(otherPortal.SpawnPoint.position);
+            player.transform.rotation = otherPortal.SpawnPoint.rotation;
+
+        }
+
+        private Portal GetOtherPortal()
+        {
+            var portals = FindObjectsOfType<Portal>();
+            foreach (var portal in portals)
+            {
+                if (portal != this)
+                {
+                    return portal;
+                }
+            }
+
+            return null;
+            
         }
     }
 }
