ecf9393070d3527b33aa69d2c979c86555830675
Akachan <chaves.mayra@gmail.com>
2024-02-16 14:13:35 -0300

    Player Input in Cutscenes

--- Assets/Scripts/RPG/Cinematics/CinematicsControlRemover.cs
+++ Assets/Scripts/RPG/Cinematics/CinematicsControlRemover.cs
@@ -1,25 +1,33 @@
 ﻿using System;
+using RPG.Control;
+using RPG.Core;
 using UnityEngine;
 using UnityEngine.Playables;
+
 
 namespace RPG.Cinematics
 {
     public class CinematicsControlRemover : MonoBehaviour
     {
+        private GameObject _player;
         private void Start()
         {
+            _player = GameObject.FindWithTag("Player");
             GetComponent<PlayableDirector>().played += DisableControl;
             GetComponent<PlayableDirector>().stopped += EnableControl;
         }
 
         private void DisableControl(PlayableDirector pd)
         {
-            print("DisableControl");
+            
+            _player.GetComponent<ActionScheduler>().CancelCurrentAction();
+            _player.GetComponent<PlayerController>().enabled = false;
         }
 
         private void EnableControl(PlayableDirector pd)
         {
             print("EnableControl");
+            _player.GetComponent<PlayerController>().enabled = true;
         }
     }
 }
--- Assets/Scripts/RPG/Combat/Fighter.cs
+++ Assets/Scripts/RPG/Combat/Fighter.cs
@@ -89,6 +89,7 @@ namespace RPG.Combat
         {
             StopAttackAnimation();
             _target = null;
+            GetComponent<Mover>().Cancel();
         }
 
         private void StopAttackAnimation()
