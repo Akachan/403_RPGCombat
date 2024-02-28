96e8317b79b2a6fa2a1e14a76d5ef85f8c3eb215
Akachan <chaves.mayra@gmail.com>
2024-02-28 18:46:59 -0300

    Capturing MultipleParameters

--- Assets/Scripts/RPG/Movement/Mover.cs
+++ Assets/Scripts/RPG/Movement/Mover.cs
@@ -1,4 +1,5 @@
 
+using System.Collections.Generic;
 using RPG.Core;
 using RPG.Saving;
 using UnityEngine;
@@ -54,18 +55,30 @@ namespace RPG.Movement
             _navMeshAgent.isStopped = true;
         }
 
-
+        [System.Serializable]
+        struct MoverSaveData
+        {
+            public SerializableVector3 position;
+            public SerializableVector3 rotation;
+        }
         public object CaptureState()
         {
-            return new SerializableVector3(transform.position);
+            MoverSaveData data = new MoverSaveData();
+            data.position = new SerializableVector3(transform.position);
+            data.rotation= new SerializableVector3(transform.eulerAngles);
+            return data;
         }
 
         public void RestoreState(object state)
         {
-            SerializableVector3 position = (SerializableVector3)state;
+            MoverSaveData data = new MoverSaveData();
+            data = (MoverSaveData)state;
+            
             _navMeshAgent.enabled = false;
-            transform.position = position.ToVector();
+            transform.position = data.position.ToVector();
             _navMeshAgent.enabled = true;
+           
+            transform.eulerAngles =data.rotation.ToVector();
         }
     }
 }
